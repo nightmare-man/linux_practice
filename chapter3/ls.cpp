3af4ea1dff726cf62ab56d7ebae05b5babe03897
nightmare-man <107lsmlsm@whut.edu.cn>
2023-03-22 21:43:01 +0800

    add sort

--- chapter3/ls.cpp
+++ chapter3/ls.cpp
@@ -2,16 +2,28 @@
 #include <dirent.h>
 #include <iostream>
 #include <errno.h>
+#include <vector>
+#include <algorithm>
+#include <string.h>
 using namespace std;
+void do_ls(const char* name){
+    DIR* s=opendir(name);
+    if(!s) {
+        cout<<"can't open dir :"<<name<<endl;
+        return;
+    }
+    vector<dirent*> v{};
+    for(dirent* p=nullptr;p=readdir(s);){
+        v.push_back(p);
+    }
+    sort(v.begin(),v.end(),[](dirent* a,dirent* b){
+        return strcmp(a->d_name,b->d_name)<0;
+    });
+    for(auto x:v) cout<<x->d_name<<endl;
+    return;
+}
 int main(int ac,char*av[]){
     if(ac<2) return 0;
-    DIR* s=opendir(av[1]);
-    if(!s) {
-        cout<<"can't open dir :"<<av[1]<<endl;
-        return -1;
-    }
-    for(dirent* p=nullptr;p=readdir(s);){
-        cout<<p->d_name<<endl;
-    }
+    while(ac>1) do_ls(av[--ac]);
     return 0;
 }

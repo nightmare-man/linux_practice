17cf4a073764c2d42056ed6649e433cc49d35c0f
nightmare-man <107lsmlsm@whut.edu.cn>
2023-03-22 22:49:11 +0800

    用掩码解读mode各个位

--- chapter3/ls.cpp
+++ chapter3/ls.cpp
@@ -10,6 +10,35 @@
 #include <iomanip>
 #include <time.h>
 using namespace std;
+//stat结构体里的st_mode字段是一个16位数，用位来编码
+//信息，因此要用掩码与运算获取对应位，定义在stat.h里
+string show_mode_info(int mode){
+    string priv_str{"---------"};
+    int filetype=mode&S_IFMT;
+    switch (filetype)
+    {
+    case S_IFDIR:
+        priv_str[0]='d';
+        break;
+    case S_IFBLK:
+        priv_str[0]='b';
+        break;
+    case S_IFREG:
+        priv_str[0]='f';
+    default:
+        break;
+    }
+    if(mode&S_IRUSR) priv_str[1]='r';
+    if(mode&S_IWUSR) priv_str[2]='w';
+    if(mode&S_IXUSR) priv_str[3]='x';
+    if(mode&S_IRGRP) priv_str[4]='r';
+    if(mode&S_IWGRP) priv_str[5]='w';
+    if(mode&S_IXGRP) priv_str[6]='x';
+    if(mode&S_IROTH) priv_str[7]='r';
+    if(mode&S_IWOTH) priv_str[8]='w';
+    if(mode&S_IXOTH) priv_str[9]='x';
+    return priv_str;
+}
 void show_file_info(const char* fp){
     struct stat tmp;
     int ret=stat(fp,&tmp);
@@ -17,6 +46,7 @@ void show_file_info(const char* fp){
     char* time=ctime(&tmp.st_mtim.tv_sec);\
     cout<<fp<<"\n";
     cout<<setw(8)<<"mode:\t"<<tmp.st_mode<<"\n";
+    cout<<setw(8)<<"mode:\t"<<show_mode_info(tmp.st_mode)<<"\n";
     cout<<setw(8)<<"links:\t"<<tmp.st_nlink<<"\n";
     cout<<setw(8)<<"user:\t"<<tmp.st_uid<<"\n";
     cout<<setw(8)<<"size:\t"<<tmp.st_size<<"\n";

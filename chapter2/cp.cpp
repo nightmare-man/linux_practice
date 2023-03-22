31d884046b77103339d9f560583b551f2fec45c4
nightmare-man <107lsmlsm@whut.edu.cn>
2023-03-22 12:20:44 +0800

    fstream带缓冲

--- chapter2/cp.cpp
+++ chapter2/cp.cpp
@@ -9,6 +9,7 @@ int main(int ac,char* av[]){
         cout<<"need 2 argument"<<endl;
         return 0;
     }
+    //ifstream 和ofstream都带用户层缓冲
     ifstream from{av[1],ios::binary};
     ofstream to{av[2],ios::out|ios::trunc};
     if(!from) throw runtime_error("can't open file"+string{av[1]});

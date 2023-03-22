b3ecf325665f74e1fb4fb3e6ccc9d4fb00006063
nightmare-man <107lsmlsm@whut.edu.cn>
2023-03-22 22:27:53 +0800

    格式化显示信息setw 和 setiotag

--- chapter3/ls.cpp
+++ chapter3/ls.cpp
@@ -10,6 +10,20 @@
 #include <iomanip>
 #include <time.h>
 using namespace std;
+void show_file_info(const char* fp){
+    struct stat tmp;
+    int ret=stat(fp,&tmp);
+    if(ret) throw runtime_error("can't get info ");
+    char* time=ctime(&tmp.st_mtim.tv_sec);\
+    cout<<fp<<"\n";
+    cout<<setw(8)<<"mode:\t"<<tmp.st_mode<<"\n";
+    cout<<setw(8)<<"links:\t"<<tmp.st_nlink<<"\n";
+    cout<<setw(8)<<"user:\t"<<tmp.st_uid<<"\n";
+    cout<<setw(8)<<"size:\t"<<tmp.st_size<<"\n";
+    cout<<setw(8)<<"group:\t"<<tmp.st_gid<<"\n";
+    cout<<setw(8)<<"time:\t"<<time<<"\n";
+    return;
+}
 void do_ls(const char* dir){
     DIR* s=opendir(dir);
     if(!s) {
@@ -24,18 +38,12 @@ void do_ls(const char* dir){
         return strcmp(a->d_name,b->d_name)<0;
     });
     for(auto x:v){
-        struct stat tmp;
-        string s1{dir};
-        string s2{x->d_name};
-        int ret=stat((s1+s2).c_str(),&tmp);
-        if(ret) throw runtime_error("can't get info ");
-        char* time=ctime(&tmp.st_mtim.tv_sec);
-        cout<<setw(8)<<setiosflags(ios::left)<<x->d_name<<"\t"<<time<<endl;
+        cout<<setw(8)<<setiosflags(ios::left)<<x->d_name<<endl;
     }
     return;
 }
 int main(int ac,char*av[]){
     if(ac<2) return 0;
-    while(ac>1) do_ls(av[--ac]);
+    while(ac>1) show_file_info(av[--ac]);//do_ls(av[--ac]);
     return 0;
 }

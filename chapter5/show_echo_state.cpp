343069442ee8a2498fae4aa50b1ffc8b9325f5ee
nightmare-man <107lsmlsm@whut.edu.cn>
2023-03-24 09:59:15 +0800

    写入终端属性

--- chapter5/show_echo_state.cpp
+++ chapter5/show_echo_state.cpp
@@ -3,17 +3,21 @@
 #include <iostream>
 using namespace std;
 
-int main(){
+int main(int ac,char*av[]){
+    if(ac<2) return -1;
+    bool flag=av[1][0]=='y';
     struct termios info;
     //终端驱动程序控制四种操作，输入流的操作，输出流的操作，控制程序的操作，内部字符的操作
     //这里要查看终端输入是否开启回显，因此将STDIN_FILENO传入作为fd
+    //通过man -k terminal | grep attr可以找到这两个系统调用
     int rv=tcgetattr(STDIN_FILENO,&info);
     if(rv) {
         cout<<"无法获取终端属性";
         return -1;
     }
-    int flag=info.c_lflag&ECHO;
-    if(flag) cout<<"输入流开启了回显"<<endl;
-    else cout<<"输入流未开启回显"<<endl;
+    if(flag)info.c_lflag|=ECHO;
+    else info.c_lflag&=( ~ECHO );
+    //写回属性
+    tcsetattr(STDIN_FILENO,TCSANOW,&info);
     return 0;
 }
